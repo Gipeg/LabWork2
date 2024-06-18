8e8d176e8bbea1040c89306570a721ee420b2fbf
fisp <arenatkanovna@gmail.com>
2024-06-18 23:24:06 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = { 4, 6, -2, 0, 1, -16, 8, };
+
+for (int j = 0; j < array.Length; j++)
+{
+    Console.Write($"{array[j]}\t");
+}
+Console.WriteLine();
+
+Console.WriteLine("Введите число индекса которого надо найти:");
+int number = Convert.ToInt32(Console.ReadLine());
+int position = -1;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == number)
+    {
+        position = i + 1;
+        break;
+    }
+}
+
+Console.WriteLine($"Позиция: {position}");
+
+
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Массив: ");
+int[] numbers = new int[] { 97, 45, 32, 65, 83, 23, 15 };
+Array.Sort(numbers);
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+
+int left = 0;
+int right = numbers.Length -1;
+while (left <= right)
+{
+
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+
+}
+
+Console.WriteLine("Результат: ");
